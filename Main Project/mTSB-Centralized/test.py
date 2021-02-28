f797e378aeb55e6f11344ef38e5107e1739c0b3b
Rasmus <rasmus123102@gmail.com>
2021-02-28 01:01:28 +0100

    ADD: DPX Test Script

---
+++ Main Project/mTSB-Centralized/test.py
@@ -0,0 +1,87 @@
+
+def breed():
+    parent1_continuous = [1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0, 10]
+    parent2_continuous = [4, 6, 5, 0, 10, 0, 7, 8, 9, 0, 1, 2, 3]
+
+    # --- CROSSOVER OPERATOR (DPX) ---
+    child1 = []
+    match_list = []
+    fragment = []
+    fragment_list = []
+    remainder = []
+    considerations = []
+
+    # Keeping track of the breakpoints for both parents
+    p1_breakpoints = [i for i in range(len(parent1_continuous)) if parent1_continuous[i] == 0]
+    p2_breakpoints = [i for i in range(len(parent2_continuous)) if parent2_continuous[i] == 0]
+
+    # Compare element i in gene 1 with all elements in gene 2
+    # If element i in gene1 is found in gene 2 return the index at which the match occurred
+    for i in range(len(parent1_continuous)):
+        for k in range(len(parent2_continuous)):
+            if parent1_continuous[i] == parent2_continuous[k] and parent1_continuous[i] != 0:
+                match_list.append(k)
+                break
+
+    # Check if the matches occurred in sequence and if they did add them as a fragment
+    former_match = False
+    for i in range(len(match_list)):
+        if i < len(match_list)-1:
+            if match_list[i+1] == match_list[i]+1 or match_list[i+1] == match_list[i]-1:
+                former_match = True
+                fragment.append(parent2_continuous[match_list[i]])
+
+            else:
+                if former_match:
+                    fragment.append(parent2_continuous[match_list[i]])
+                    fragment.append(0)
+                    former_match = False
+        else:
+            if former_match:
+                fragment.append(parent2_continuous[match_list[i]])
+
+    # Find the tasks that are not part of a common sequence in both parents
+    remainder = [task for task in parent2_continuous if task not in fragment]
+
+    # Order the fragments in a nested list for reconstruction
+    sub_list = []
+    for sub_fragment in fragment:
+        if sub_fragment != 0:
+            sub_list.append(sub_fragment)
+        else:
+            fragment_list.append(sub_list)
+            sub_list = []
+
+    for sub_fragment in remainder:
+        fragment_list.append([sub_fragment])
+
+    # Points to consider for greedy reconstruction
+    for points in fragment_list:
+        if len(points) == 1:
+            considerations.append(points[0])
+        else:
+            considerations.append(points[0])
+            considerations.append(points[-1])
+
+    # TODO select random number from consideration and find the endpoint of the fragment associated with it.
+    # TODO Find out which task has the smallest distance to endpoint and connect the fragment containing the task
+    # TODO If the endpoint of one fragments has the shortest distance to the endpoint of another, reverse and connect the other
+    # TODO Remember to remove the task considerations after connecting their associated fragment
+
+    print("Parent 1: ", parent1_continuous)
+    print("Parent 2: ", parent2_continuous)
+    print("Match list: ", match_list)
+    print("\n")
+    print("Fragment List")
+    for elements in fragment_list:
+        print(elements)
+    print("\nPoints for Reconstruction: ", considerations)
+
+
+
+breed()
+
+
+
+
+

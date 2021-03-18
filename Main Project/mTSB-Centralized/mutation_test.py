57ff86217edfa2eef7b96f55e99bc85e0440cf76
Ditte <dalber16@student.aau.dk>
2021-03-18 10:28:14 +0100

    FIX: CC no longer chooses the same index if agent length is the same

--- Main Project/mTSB-Centralized/mutation_test.py
+++ Main Project/mTSB-Centralized/mutation_test.py
@@ -3,10 +3,13 @@ import random
 # individual = [[(28, 246), (153, 250), (277, 187), (67, 143), (60, 342)],
 #               [(252, 263), (270, 24), (490, 127), (203, 296), (111, 49)],
 #               [(433, 195), (391, 466), (187, 164), (490, 30), (316, 394)]]
-# individual = [[(41, 461), (220, 215), (27, 149), (115, 32), (462, 293)],
+#individual = [[(41, 461), (220, 215), (27, 149), (115, 32), (462, 293)],
               # [(171, 217), (385, 64)],
               # [(100, 141), (194, 84), (305, 333)],
               # [(112, 307), (68, 366), (389, 296), (346, 303)]]
+
+# individual = [[(418,278), (415,335), (417,216), (468,211), (360,355), (469,190), (325,394), (381,127), (496,429), (247,224), (218,247), (321,92)],
+#               [(222,360), (450,15), (381,1), (109,229), (108,211), (116,115), (114,472), (60,166), (144,10), (12,270), (14,110), (46,14)], [(112, 307), (68, 366), (389, 296), (346, 303)]]
 
 # individual2 = [[(41, 461), (220, 215), (27, 149), (115, 32), (462, 293)],
 #               [(171, 217), (385, 64)],
@@ -45,6 +48,7 @@ def partition_insertion(individual):
     genome1, genome2 = random.sample(individual, 2)
 
     if len(genome1) > 1:
+        print("Part Inserting ------------------------------")
         start_index = random.randint(0, len(genome1) - 1)
 
         gene = genome1[start_index]
@@ -68,26 +72,30 @@ def swap(individual):
     return individual
 
 def chromosome_contraction(individual):
-    agent = [agent for agent in individual]
-    s_agent = []
-    # creates a list of the different lengths in individual
-    for i in range(0, len(individual)):
-        s_agent.append(len(agent[i]))
-    # sorts the list to find the smallest and then remember the two firsts index
-    sort_list = sorted(s_agent)
-    first_agent = s_agent.index(sort_list[0])
-    second_agent = s_agent.index(sort_list[1])
-    # print(s_agent)
-    # unpacks each list
-    genes = [genes for genes in agent[first_agent]]
-    genes2 = [genes2 for genes2 in agent[second_agent]]
-    # inserts each gene one by one from back of the first list to the start of the second list
-    genes2.extend(genes)
-    # for j in range(s_agent[first_agent]-1, -1, -1):
-    #     genes2.insert(0, genes[j])
-    # The main list is then updated and the smallest agent is deleted
-    individual[second_agent] = genes2
-    individual.pop(individual.index(agent[first_agent]))
+    if len(individual) > 2:
+        print("Contraction individual ----------------------")
+        agent = [agent for agent in individual]
+        s_agent = []
+        # creates a list of the different lengths in individual
+        for i in range(0, len(individual)):
+            s_agent.append(len(agent[i]))
+        # sorts the list to find the smallest and then remember the two firsts index
+        sort_list = sorted(s_agent)
+        first_agent = s_agent.index(sort_list[0])
+        second_agent = s_agent.index(sort_list[1])
+        if first_agent == second_agent:
+            second_agent += 1
+        # unpacks each list
+        genes = [genes for genes in agent[first_agent]]
+        genes2 = [genes2 for genes2 in agent[second_agent]]
+        # inserts each gene one by one from back of the first list to the start of the second list
+        genes2.extend(genes)
+        # for j in range(s_agent[first_agent]-1, -1, -1):
+        #     genes2.insert(0, genes[j])
+        # The main list is then updated and the smallest agent is deleted
+
+        individual[second_agent] = genes2
+        individual.pop(individual.index(agent[first_agent]))
 
     return individual
 
@@ -160,13 +168,14 @@ def insertion(individual):
     k = random.randint(0, len(individual) - 1)
     #  Splits a random agent into genes and chooses randomly one
     agent = [gene for gene in individual[k]]
-    gene = agent[random.randint(0, len(agent) - 1)]
-    #  The gene is then removed from the list
-    agent.pop(agent.index(gene))
-    #  insert(index, elem) places the gene randomly in the agent and push everything to the right
-    agent.insert(random.randint(0, len(agent) - 1), gene)
-    #  Inserts the new agent the same place it was taken from
-    individual[k] = agent
+    if len(agent) > 1:
+        gene = agent[random.randint(0, len(agent) - 1)]
+        #  The gene is then removed from the list
+        agent.pop(agent.index(gene))
+        #  insert(index, elem) places the gene randomly in the agent and push everything to the right
+        agent.insert(random.randint(0, len(agent) - 1), gene)
+        #  Inserts the new agent the same place it was taken from
+        individual[k] = agent
 
     return individual
 
@@ -189,11 +198,11 @@ def transposition(individual):
 
 
 
-#print(individual)
+# print(individual)
 # print(insertion(individual))
 # print(transposition(individual))
 # print(sequence_inversion(individual))
-#print(chromosome_contraction(individual))
+# print(chromosome_contraction(individual))
 # print(random_chromosome_contraction(individual))
 # print(chromosome_partition_max(individual))
 # print(partition_insertion(individual))
